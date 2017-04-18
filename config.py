ff3a0a04ec7411c00a3f05283545e179b00ee756
宋大才 <songdacai@songdacaideMBP.lan>
2017-04-18 16:11:43 +0800

    modify ajax

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'mysql://root:sdc@localhost/account?charset=utf8'
 SQLALCHEMY_MIGRATE_REOP = os.path.join(basedir, 'db_repository')
-
+SQLALCHEMY_TRACK_MODIFICATIONS = True
